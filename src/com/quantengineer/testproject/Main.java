94efce1ce9a168a8661041259d5389303578c6c1
musa-devop <sambocharles@outlook.com>
2020-12-02 14:13:08 +0100

    updated main file

--- src/com/quantengineer/testproject/Main.java
+++ src/com/quantengineer/testproject/Main.java
@@ -3,6 +3,9 @@ package com.quantengineer.testproject;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Welcome to Java!");
+
+        System.out.println("Welcome to Java!");
+        System.out.println("Java is a nice language");
     }
+
 }
